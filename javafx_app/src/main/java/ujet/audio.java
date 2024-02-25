502da39fe87b57f5a24d439e2496e022fc7097f7
ajh6754 <ajh6754@g.rit.edu>
2024-02-24 19:29:43 -0500

    With this commit, there is not much in terms of progress. Must switch to windows to make things work as intended. Still, AudioSystem is not usable, so maybe there's somethign there.

--- javafx_app/src/main/java/ujet/audio.java
+++ javafx_app/src/main/java/ujet/audio.java
@@ -1,5 +1,26 @@
 package ujet;
 
+import javax.sound.sampled.*;
+
+import javafx.fxml.FXML;
+
+// if we can somehow access the software mixer, we should be able to have access to the apps sending info to the mixer?
+
 public class audio {
-    
+    protected Mixer.Info[] mixer_info = AudioSystem.getMixerInfo();
+    //protected Line[] lines = AudioSystem.getMixer(mixer_info[0]).getTargetLines();
+
+    @FXML
+    private void getInfo()
+    {
+        //System.out.println(lines[0].toString());
+        //System.out.println(lines.length);
+
+        for(int i = 0; i < mixer_info.length; i++)
+        {
+            Line[] lines = AudioSystem.getMixer(mixer_info[i]).getTargetLines();
+            System.out.println(mixer_info[i].toString());
+            System.out.println(lines.length);
+        }
+    }
 }
